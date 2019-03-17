649a191137a60dc10958619769cf709663f482c3
Ondrej Zeman <ozeman77@gmail.com>
2019-03-17 14:30:35 +0100

    description

--- Acronymizer/AcronymParser.cs
+++ Acronymizer/AcronymParser.cs
@@ -4,6 +4,10 @@ using System.Linq;
 
 namespace Acronymizer
 {
+	/*
+	 * I would do the extension for user defined symbols either like this with state or with functional approach.
+	 * My createAcronym would have function as parameter. This function would be called on every split string so that user can replace symbols on his own.
+	 */
 	public class AcronymParser
 	{
 		public List<(string ReplaceWord, string ReplaceWith)> ReplaceOpts { get; private set; }
@@ -13,12 +17,12 @@ namespace Acronymizer
 			List<(string ReplaceWord, string ReplaceWith)> opts = null)
 		{
 			SplitOps = splitOpts ?? new List<char> {' ', '-'};
-			ReplaceOpts = opts ?? new List<(string , string )>();
+			ReplaceOpts = opts ?? new List<(string, string )>();
 		}
 
 
 		private IEnumerable<string> ParseWords(string input) =>
-			input.Split(SplitOps.ToArray()).Select(str =>
+			input.Split(SplitOps.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(str =>
 				ReplaceOpts.Aggregate(str, (acc, opt) =>
 					acc.Replace(opt.ReplaceWord, opt.ReplaceWith))
 			);
